95d0d2a5aece1f80e311bb3519945428bc0d8fa7
Brett Lazarine <jblazarine@gmail.com>
2023-01-17 10:16:06 -0600

    create variables and print to screen

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Brett";
+            int age = 31;
+            char letter = 'Z';
+            bool truth = true;
+            double timeToGetHired = 1.0;
+            decimal earnings = 1000000000.9735m;
+            //Practicing line breaks. May come back around and refactor down the line if I find a cleaner way of doing it.
+            Console.WriteLine($"My name is {name}.\nI am {age} years old.\nI aim to learn coding, from A all the way to {letter}.\nAnd you can count on that being {truth.ToString().ToLower()}.\nHopefully I'll land a gig within {timeToGetHired} year of when I started out.\nSure would be cool if all this learning ended up earning me ${earnings}...");
         }
     }
 }
